28902cb3e1a096d078f8dc9eb940bd7b0c1c4139
Yvo Cilon <yvo.cilon@appcore.nl>
2020-02-12 09:54:37 +0200

    add task

---
+++ model/task.ts
@@ -0,0 +1,25 @@
+import * as mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const TaskSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  lane: {
+    type: String,
+    required: true
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  }
+});
+
+export interface Task extends mongoose.Document {
+  title: string;
+  lane: string;
+  id: string;
+}
+
+export const TaskModel = mongoose.model<Task>("task", TaskSchema);
